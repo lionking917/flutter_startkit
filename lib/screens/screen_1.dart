0d8e4acd403a19e7f836a16194b6cbdf9b5d8175
lionking917 <lionking917@outlook.com>
2021-03-26 23:21:38 -0700

    Add changes two screens

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:test_prj/constant/colors.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({Key key}) : super(key: key);
@@ -6,5 +9,96 @@ class Screen1 extends StatelessWidget {
   static const routeName = '/screen_1';
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => Container(
+      color: Color(0xFFE5E5E5),
+      padding: EdgeInsets.only(left: 16, right: 16),
+      child: SafeArea(
+          child: Center(
+              child: Container(
+                  constraints: BoxConstraints(
+                      maxHeight: MediaQuery.of(context).size.height - 48),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    color: ProjectColor.white,
+                  ),
+                  child: Wrap(
+                    children: [
+                      Stack(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                    top: 16, left: 16, right: 16),
+                                child: Text('Title',
+                                    style: TextStyle(
+                                        color: ProjectColor.black,
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              Container(
+                                margin: EdgeInsets.all(16),
+                                child: Text(
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.',
+                                    style: TextStyle(
+                                        color: ProjectColor.black,
+                                        fontSize: 14)),
+                              ),
+                              IntrinsicHeight(
+                                  child: Container(
+                                      constraints:
+                                          BoxConstraints(maxHeight: 400),
+                                      child: Scrollbar(
+                                        child: SingleChildScrollView(
+                                            scrollDirection: Axis.vertical,
+                                            child: Container(
+                                              padding: EdgeInsets.all(16),
+                                              color: Color(0xFFDFDEDE),
+                                              child: Text(
+                                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.Duis convallis justo non nisi placerat maximus.',
+                                                  style: TextStyle(
+                                                      color: ProjectColor.black,
+                                                      fontSize: 14)),
+                                            )),
+                                      ))),
+                              Container(
+                                  padding: EdgeInsets.all(16),
+                                  child: SizedBox(
+                                      width: 140,
+                                      height: 44,
+                                      child: ElevatedButton(
+                                          child: Text('TEST BUTTON',
+                                              style: TextStyle(
+                                                  color: ProjectColor.white,
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.bold)),
+                                          style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all<
+                                                      Color>(Color(0xFFDB00FF)),
+                                              shape: MaterialStateProperty.all<
+                                                      RoundedRectangleBorder>(
+                                                  RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(100),
+                                              ))),
+                                          onPressed: () {})))
+                            ],
+                          ),
+                          Positioned(
+                              top: 10,
+                              right: 10,
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Icon(
+                                    Icons.close,
+                                    color: ProjectColor.black,
+                                    size: 30,
+                                  )))
+                        ],
+                      )
+                    ],
+                  )))));
 }
--- lib/screens/screen_2.dart
+++ lib/screens/screen_2.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:test_prj/constant/colors.dart';
+import 'package:test_prj/providers/base_items_provider.dart';
+import 'package:test_prj/providers/horizontal_items.dart';
+import 'package:test_prj/providers/vertical_items.dart';
 
 class Screen2 extends StatelessWidget {
   const Screen2({Key key}) : super(key: key);
@@ -6,5 +12,118 @@ class Screen2 extends StatelessWidget {
   static const routeName = '/screen_2';
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) {
+    var horizontalItemsProvider = new HorizontalItemsProvider();
+    var verticalItemsProvider = new VerticalItemsProvider();
+
+    return Container(
+        color: ProjectColor.white,
+
+        child: SafeArea(
+            child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 76),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 16),
+                    child: Text(
+                        'Section 1 (${horizontalItemsProvider.itemsCount})',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: ProjectColor.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Scrollbar(
+                      child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: getWidgets(context, horizontalItemsProvider))),
+                  Container(
+                    margin: EdgeInsets.only(left: 16),
+                    child: Text(
+                        'Section 2 (${verticalItemsProvider.itemsCount})',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: ProjectColor.black,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold)),
+                  ),
+                  Expanded(
+                      child: Scrollbar(
+                          child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: getWidgets(context, verticalItemsProvider,
+                                  isHorizontal: false))))
+                ],
+              ),
+            ),
+            Positioned(
+                top: 28,
+                right: 10,
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.close,
+                      color: ProjectColor.black,
+                      size: 30,
+                    )))
+          ],
+        )));
+  }
+
+  Widget getWidgets(BuildContext context, BaseItemsProvider itemsProvider,
+      {bool isHorizontal = true}) {
+    List<Widget> list = <Widget>[];
+    for (var i = 0; i < itemsProvider.itemsCount; i++) {
+      list.add(Container(
+        width: isHorizontal ? 350 : MediaQuery.of(context).size.width - 32,
+        height: 150,
+        margin: EdgeInsets.only(
+            left: isHorizontal
+                ? i == 0
+                    ? 16
+                    : 8
+                : 16,
+            right: isHorizontal
+                ? i == itemsProvider.itemsCount - 1
+                    ? 16
+                    : 8
+                : 16,
+            top: isHorizontal
+                ? 16
+                : i == 0
+                    ? 16
+                    : 8,
+            bottom: isHorizontal
+                ? 16
+                : i == itemsProvider.itemsCount - 1
+                    ? 16
+                    : 8),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: ProjectColor.white,
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+          boxShadow: [
+            BoxShadow(
+                color: ProjectColor.black.withOpacity(0.25), blurRadius: 16),
+          ],
+        ),
+        child: Text(itemsProvider.generateItemAt(i + 1),
+            style: TextStyle(
+                color: ProjectColor.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold)),
+      ));
+    }
+    if (isHorizontal) {
+      return Row(children: list);
+    } else {
+      return Column(children: list);
+    }
+  }
 }
